825c93741d1a7dc57f55e7ef9dadd1e5f26214bb
Moh Ariful <nextkoding@gmail.com>
2022-12-13 16:17:57 +0700

    Adding read function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Create from './components/Create';
+import Read from './components/Read';
+
 
 function App() {
   return (
@@ -7,8 +9,13 @@ function App() {
       <div>
       <h1>React CRUD Operations Using Axios</h1>
       </div>
+
       <div>
       <Create />
+      </div>
+
+      <div style={{marginTop: 30}}>
+        <Read />
       </div>
       
     </div>
---
+++ src/components/Read.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { Button, Table } from "semantic-ui-react";
+import axios from "axios";
+
+export default function Read() {
+  const [apiData, setApiData] = useState([]);
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/").then((getData) => {
+      setApiData(getData.data);
+    });
+  });
+  return (
+    <div>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>ID</Table.HeaderCell>
+            <Table.HeaderCell>Nama Siswa</Table.HeaderCell>
+            <Table.HeaderCell>Asal Siswa</Table.HeaderCell>
+            <Table.HeaderCell>Edit</Table.HeaderCell>
+            <Table.HeaderCell>Hapus</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {apiData.map((data) => {
+            return (
+              <Table.Row>
+                <Table.Cell>{data.id} </Table.Cell>
+                <Table.Cell>{data.name} </Table.Cell>
+                <Table.Cell>{data.asal} </Table.Cell>
+                <Table.Cell>
+                    <Button>Edit</Button>
+                </Table.Cell>
+                <Table.Cell>
+                    <Button>Hapus</Button>
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </div>
+  );
+}
